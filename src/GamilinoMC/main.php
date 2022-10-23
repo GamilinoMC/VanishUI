709f78b62d50daacb4761505c11660b613e282d2
GamilinoMC <81467242+GamilinoMC@users.noreply.github.com>
2022-10-23 18:07:17 +0200

    Create main.php

---
+++ src/GamilinoMC/main.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace GamilinoMC;
+
+use jojoe77777\FormAPI\SimpleForm;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+<?php
+
+namespace PinguGames;
+
+use jojoe77777\FormAPI\CustomForm;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\GameMode;
+use pocketmine\player\Player;
+use jojoe77777\FormAPI\SimpleForm;
+use pocketmine\command\defaults\KickCommand;
+use pocketmine\event\player\PlayerKickEvent;
+
+class main extends PluginBase
+{
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool
+    {
+        if ($cmd->getName() === "vanish") {
+            if($sender instanceof Player){
+                $this->VanishUI($sender);
+            }
+            return true;
+        }
+    return true;
+    }
+
+    public function VanishUI(Player $player){
+        $form = new SimpleForm(function (Player $player, $data = null){
+            if ($data === null){
+                return;
+            }
+            switch ($data) {
+                case 0:
+                    $player->setInvisible(true);
+                    $player->setSilent(true);
+                    $player->setFlying(true);
+                    $player->setAllowFlight(true);
+                    $player->sendPopup("§aYou are vanished now!");
+                    break;
+
+                case 1:
+                    $player->setInvisible(false);
+                    $player->setSilent(false);
+                    $player->setFlying(false);
+                    $player->setAllowFlight(false);
+                    $player->sendPopup("§cYou have been unvanished!");
+                    break;
+            }
+        });
+        $form->setTitle("§bVanish §eUI");
+        $form->setContent("§2Choose wether you want to be Vanished or not");
+        $form->addButton("§a§lVanish");
+        $form->addButton("§c§lUnvanish");
+        $form->sendToPlayer($player);
+        return $form;
+    }
+}
